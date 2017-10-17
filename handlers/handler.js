4eeadd97b10f0a5f0ee9b6ffd2c9c573ccc696d7
Andrew Hammond <andrew@tremorlab.com>
2017-10-17 16:25:41 -0400

    Stub out basic read endpoint

--- handlers/handler.js
+++ handlers/handler.js
@@ -1,6 +1,13 @@
-const AWS = require('aws-sdk');
+const AWS = require ('aws-sdk');
 
-var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+var dynamodb = new AWS.DynamoDB ({apiVersion: '2012-08-10'});
+
+var respHeaders = {
+  // for CORS... later
+  'Access-Control-Allow-Credentials': false,
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Origin': 'https://favi.andrhamm.com',
+};
 
 // See https://github.com/stewartlord/serverless-ruby
 // const spawn = require('child_process').spawn;
@@ -36,68 +43,118 @@ var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 //   });
 // }
 
-module.exports.createFave = function(event, context, callback) {
-  // rubyproxy('./create.rb', event, context, callback);
-  
-  console.log("createFave func");
-  console.log(event);
+module.exports.readFave = function (event, context, callback) {
+  console.log ('readFave func');
+  console.log (event);
 
-  var reqBody = JSON.parse(event.body);
+  var hostname = event.queryStringParameters.hostname;
+  var hostnameParsed;
 
-  var respHeaders = {
-    // for CORS... later
-    "Access-Control-Allow-Credentials": false,
-    "Access-Control-Allow-Headers": "Content-Type"
-  };
-
-  var hostnameAttr;
-  
-  if (reqBody.hostname) {
-    const URL = require('url');
-    var url = URL.parse(reqBody.hostname);
+  if (hostname) {
+    const URL = require ('url');
+    var url = URL.parse (hostname);
 
     if (url.hostname) {
-      hostnameAttr = url.protocol + "//" + url.hostname;
+      hostnameParsed = url.protocol + '//' + url.hostname;
 
       if (url.protocol && (url.protocol == 'http' || url.protocol == 'https')) {
-        hostnameAttr = url.protocol + "//" + hostnameAttr;
+        hostnameParsed = url.protocol + '//' + hostnameParsed;
+      }
+    }
+  }
+
+  if (!hostnameParsed) {
+    callback (null, {
+      statusCode: 422,
+      body: JSON.stringify ({
+        message: 'Missing or invalid hostname',
+      }),
+      headers: respHeaders,
+    });
+  }
+
+  var getItem = {
+    TableName: 'FaveRecords',
+    Key: {
+      hostname: {S: hostnameParsed},
+    },
+  };
+
+  dynamodb.getItem (getItem, function (err, data) {
+    if (err) {
+      // TODO: catch errors when trying to insert the same item
+      // and return different status code with existing doc
+      console.log (err, err.stack);
+      context.fail (err);
+    }
+
+    callback (null, {
+      statusCode: 202,
+      body: JSON.stringify ({
+        parsed_hostname: hostnameParsed,
+        item: data,
+      }),
+      headers: respHeaders,
+    });
+  });
+};
+
+module.exports.createFave = function (event, context, callback) {
+  // rubyproxy('./create.rb', event, context, callback);
+
+  console.log ('createFave func');
+  console.log (event);
+
+  var reqBody = JSON.parse (event.body);
+
+  var hostnameAttr;
+
+  if (reqBody.hostname) {
+    const URL = require ('url');
+    var url = URL.parse (reqBody.hostname);
+
+    if (url.hostname) {
+      hostnameAttr = url.protocol + '//' + url.hostname;
+
+      if (url.protocol && (url.protocol == 'http' || url.protocol == 'https')) {
+        hostnameAttr = url.protocol + '//' + hostnameAttr;
       }
     }
   }
 
   if (!hostnameAttr) {
-    callback(null, {
+    callback (null, {
       statusCode: 422,
-      body: JSON.stringify({
-        message: "Missing or invalid hostname"
+      body: JSON.stringify ({
+        message: 'Missing or invalid hostname',
       }),
-      headers: respHeaders
+      headers: respHeaders,
     });
   }
 
   var putItem = {
-    TableName: "FaveRecords",
+    TableName: 'FaveRecords',
     Item: {
-      "hostname": { S: hostnameAttr },
-      "created_at": { N: (new Date).getTime().toString() }
+      hostname: {S: hostnameAttr},
+      created_at: {N: new Date ().getTime ().toString ()},
     },
-    ConditionExpression: "attribute_not_exists(hostname)"
+    ConditionExpression: 'attribute_not_exists(hostname)',
   };
 
-  dynamodb.putItem(putItem, function(err, data) {
-      if (err) {
-        // TODO: catch errors when trying to insert the same item
-        // and return different status code with existing doc
-        console.log(err, err.stack);
-        context.fail(err);
-      }
-  });
+  dynamodb.putItem (putItem, function (err, data) {
+    if (err) {
+      // TODO: catch errors when trying to insert the same item
+      // and return different status code with existing doc
+      console.log (err, err.stack);
+      context.fail (err);
+    }
 
-  callback(null, {
-    statusCode: 202,
-    body: JSON.stringify({
-      "parsed_hostname": hostnameAttr
-    }),
-    headers: respHeaders
-  });  
-}
+    callback (null, {
+      statusCode: 202,
+      body: JSON.stringify ({
+        parsed_hostname: hostnameAttr,
+      }),
+      headers: respHeaders,
+    });
+  });
+};
